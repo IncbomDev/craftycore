453d64c923a885241e71be62ba15eec170c6b088
Incbom <99501939+IncbomDev@users.noreply.github.com>
2022-11-18 21:25:33 -0500

    Wipe for Pillage_ to upload maven configs
    Co-Authored-By: PillageDev <63556914+PillageDev@users.noreply.github.com>

--- src/main/java/org/craftystudios/utils/Settings.java
+++
@@ -1,8 +0,0 @@
-package org.craftystudios.utils;
-public class Settings {
-//UPDATE THESE VALUES WHEN NEW VERSION IS RELEASED!!!!!!!!!!!!!!!!!!!!
-
-
-    public static String PLUGIN_URL = "https://spigotmc.org/resources/SOMENUMBER";
-    public static String VERSION = "0.1";
-}
