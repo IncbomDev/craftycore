7a68141baf0251a778bba6493f8f35be3db864c7
PillageDev <pillageyt@gmail.com>
2022-11-18 21:28:59 -0500

    initial

---
+++ src/main/java/org/craftystudios/commands/CraftyCoreCommand.java
@@ -0,0 +1,21 @@
+package org.craftystudios.commands;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.craftystudios.utils.Settings;
+
+public class CraftyCoreCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            player.sendMessage("§6§lCraftyStudios §7§l» §f§lRunning on version 1.0");
+            return false;
+        }
+        return false;
+    }
+}
